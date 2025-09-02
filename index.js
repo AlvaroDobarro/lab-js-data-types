1a04f2d614bfe5ffb9d1d75ca11f3dd03704d349
Alvaro <adt2708@gmail.com>
2025-09-02 21:42:13 +0200

    Solved lab

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 /*******************************************
     Iteration 1.1 | Tongue Twister
 *******************************************/
@@ -6,6 +8,10 @@ const s2 = "fed";
 const s3 = "Ted";
 const s4 = "bread";
 const s5 = "and";
+
+const tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s3 + " " + s2 + " " + s1 + " " + s4;
+
+console.log(tongueTwister)
 
 // Concatenate the string variables into one new string
 
@@ -21,6 +27,12 @@ const s5 = "and";
 const part1 = "java";
 const part2 = "script";
 
+const result = part1.slice(0, -1) + part1.slice(-1).toUpperCase() +
+               part2.slice(0, -1) + part2.slice(-1).toUpperCase();
+
+console.log(result);
+
+
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
 
@@ -34,6 +46,10 @@ const part2 = "script";
 *******************************************/
 const billTotal = 84;
 
+const tipAmount = billTotal * 0.15;
+
+console.log(tipAmount);
+
 // Calculate the tip (15% of the bill total)
 
 
@@ -45,14 +61,14 @@ const billTotal = 84;
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
-
+const randomNumber = Math.floor(Math.random() * 10)
 // Generate a random integer between 1 and 10 (inclusive)
 
 
 // Print the generated random number
 
 
-
+console.log(randomNumber);
 /*******************************************
     Iteration 3.1 | Booleans
 *******************************************/
@@ -74,3 +90,11 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
